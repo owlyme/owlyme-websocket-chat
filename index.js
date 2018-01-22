4c78a6188e0f4da1718b9dbfd96a642a5ff7c344
owlyme <chudequan_48@163.com>
2018-01-22 14:40:53 +0800

    owlyme-chat

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+let clientList=[]
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id);
+    // Handle chat event
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+    // client login
+    socket.on('login', function(data){
+        clientList.push( data );
+        //client login sucess     
+        let _index = clientList.indexOf( data)
+        if( _index >=0 ){
+            clientList.push( data );
+            socket.emit('sucess', '登录成功！');
+        }
+
+        socket.broadcast.emit('login', data);
+    });
+
+    // client close
+    socket.on('logout', function(data){
+        let _index = clientList.indexOf( data)
+        if( _index >=0 ){
+            clientList.splice( _index, 1 );
+            socket.broadcast.emit('logout', data);
+        }
+    });
+
+    // socket.on('disconnect', function(data){
+    //     socket.broadcast.emit('logout', );
+    // });
+});
---
+++ public/chat.js
@@ -0,0 +1,71 @@
+// Make connection
+var socket = io.connect('http://localhost:4000');
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
+      login = document.getElementById('login');
+      logout = document.getElementById('logout');
+
+function islogin(fn){  if( handle.value.trim() ){ fn() }  }
+
+// Emit events
+//login
+login.addEventListener('click', function(){
+  islogin( function(){
+    socket.emit('login', handle.value);
+  })
+});
+
+//logout
+logout.addEventListener('click', function(){
+    socket.emit('logout', handle.value);
+    handle.value = ''
+});
+//send
+btn.addEventListener('click', function(){
+  islogin( function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+    message.value = "";
+  })
+});
+//typing 
+message.addEventListener('keypress', function(){
+  islogin( function(){
+    socket.emit('typing', handle.value);
+  } )
+})
+
+// Listen for events
+socket.on('chat', function(data){
+    feedback.innerHTML = '';
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
+
+// login
+socket.on('login', function(data){
+    output.innerHTML += '<p>'+ data+ ' is login </p>';
+});
+
+// logout
+socket.on('logout', function(data){
+    output.innerHTML += '<p>'+ data+ ' is logout </p>';
+});
+
+// logout
+socket.on('sucess', function(data){
+    alert(data)
+});
+
+
+
